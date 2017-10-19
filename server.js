e52dbec1f3cea36a5b333eb6739251e0ac73c672
Yeshayahu Wasserman <Shy.Wasserman@gmail.com>
2017-10-19 11:08:37 +0300

    shy is the best

--- server.js
+++ server.js
@@ -2,13 +2,21 @@ var express = require('express');
 var app = express();
 var passport = require('passport');
 var bodyParser = require('body-parser');
-
+var LocalStrategy = require('passport-local').Strategy;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //initializes passport and tells express we wnat to use it as middleware (aka app=express)
 app.use(passport.initialize());
+
+passport.use(new LocalStrategy(function(username, password, done) {
+    if ((username === "john") && (password === "password")) {
+      return done(null, { username: username, id: 1 });
+    } else {
+      return done(null, false);
+    }
+  }));
 
 //GET routes
 app.get('/success', function (req, res) {
@@ -19,6 +27,11 @@ app.get('/login', function (req, res) {
     res.sendFile(__dirname + '/public/login.html');
 });
 
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/success',
+    failureRedirect: '/login',
+    session: false
+}));
 
 app.listen(8000, function () {
     console.log("Ready for some authentication action");
